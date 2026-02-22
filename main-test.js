7d23bd19a35bd01c7cffe8c5393a15b7e08202e1
benjyklaut <benjkutart@gmail.com>
2026-02-22 21:05:11 +0100

    fix : Correction d’une faute de frappe dans l’import et l’utilisation de , correction d’une faute de frappe dans , et ajout de la JSDoc pour la fonction .

--- main-test.js
+++ main-test.js
@@ -1,15 +1,20 @@
-import { getDependebcyTree} from './resolver.js';
+import { getDependencyTree } from './resolver.js';
 import { downloadAndExtract } from "./dowloader.js";
 import path from 'path';
 
+/**
+ * Fonction principale qui lance le processus de téléchargement et d'extraction
+ * @param {string} packageName 
+ * @param {string} version 
+ */
 async function run() {
   const REGISTERY_PATH = path.join(process.cwd(), 'NodeShare_Registery');
 
   // 1. On trouve ce qu'il faut
-  const deps = await getDependebcyTree(process.cwd());
+  const deps = await getDependencyTree(process.cwd());
 
   // 2. On télécharge tout pour (pour le test on prend juste deux pour aller vite)
-  console.console.log('🚀 Début de téléchargement des 2 premiers paquets...');
+  console.log('🚀 Début de téléchargement des 2 premiers paquets...');
   for (let i = 0; i < Math.min(deps.length, 2); i++) {
     await downloadAndExtract(deps[i], REGISTERY_PATH);
   }
